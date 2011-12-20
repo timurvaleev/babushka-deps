8a556da5eafd2574014ba8cddd7a3251829e41a9
Timur Valeev <timur.valeev@flatsoft.com>
2011-12-20 08:27:53 +0400

    first version

---
+++ remove_unneeded.rb
@@ -0,0 +1,4 @@
+dep 'remove portmap' do
+  met? { `which /sbin/portmap 2>&1`.include?('/sbin/portmap') }
+  meet { sudo `apt-get -q -y remove --purge /sbin/portmap` }
+end
---
+++ setup.rb
@@ -0,0 +1,30 @@
+#function remove_unneeded {
+#    check_remove /sbin/portmap portmap
+#
+#    check_remove /usr/sbin/rsyslogd rsyslog
+#
+#    check_remove /usr/sbin/apache2 'apache2*'
+#    check_remove /usr/sbin/named bind9
+#    check_remove /usr/sbin/smbd 'samba*'
+#    check_remove /usr/sbin/nscd nscd
+#
+#    if [ -f /usr/lib/sm.bin/smtpd ]
+#    then
+#        invoke-rc.d sendmail stop
+#        check_remove /usr/lib/sm.bin/smtpd 'sendmail*'
+#    fi
+#}
+
+#function check_remove {
+#    if [ -n "`which "$1" 2>/dev/null`" ]
+#    then
+#        DEBIAN_FRONTEND=noninteractive apt-get -q -y remove --purge "$2"
+#        print_info "$2 removed"
+#    else
+#        print_warn "$2 is not installed"
+#    fi
+#}
+
+dep 'setup' do
+  requires 'remove portmap'
+end
