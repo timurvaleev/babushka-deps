90707bc51d3db0a235d90020439f270bf868b2e9
Timur Valeev <timur.valeev@flatsoft.com>
2012-01-06 08:27:06 +0400

    test 1

--- remove_unneeded.rb
+++ remove_unneeded.rb
@@ -1,4 +1,8 @@
-dep 'remove portmap' do
-  met? { `which /sbin/portmap 2>&1`.include?('/sbin/portmap') }
-  meet { `sudo apt-get -q -y remove --purge portmap` }
+meta :remove do
+  template {
+    met? { which(basename) }
+    meet { log_shell "Removing #{basename}", "apt-get -q -y remove --purge #{basename}", :sudo => true }
+  }
 end
+
+dep 'portmap.remove'
